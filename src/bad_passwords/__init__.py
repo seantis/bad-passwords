de9cb6ececd51e6f7b143b5814e327509f48e9f1
David Salvisberg <david.salvisberg@seantis.ch>
2025-11-05 12:31:16 +0100

    Initial implementation

--- src/bad_passwords/__init__.py
+++ src/bad_passwords/__init__.py
@@ -1,3 +1,100 @@
 from __future__ import annotations
 
+import importlib.resources
+import mmap
+import warnings
+
+from hashlib import sha1
+
+
 __version__ = '0.0.0'
+__all__ = ('is_bad_password',)
+
+
+def is_bad_password(password: str) -> bool:
+    """
+    Checks the password against a set of commonly used passwords.
+
+    Returns whether or not the password is a bad password.
+    """
+    if len(password) < 10:
+        warnings.warn(
+            'Checking passwords shorter than 10 characters is not supported '
+            'they will always be considered bad passwords.',
+            category=UserWarning,
+            stacklevel=2
+        )
+        return True
+
+    pw = password.encode('utf-8')
+    length_bin = min(len(pw), 20)
+    digest = sha1(pw, usedforsecurity=False).hexdigest()
+    files = importlib.resources.files('bad_passwords') / 'passwords'
+    target_file = files / digest[0] / digest[1] / f'{length_bin}.txt'
+    if not target_file.is_file():
+        return False
+
+    with (
+        importlib.resources.as_file(target_file) as fspath,
+        fspath.open('rb', 0) as fp,
+        mmap.mmap(fp.fileno(), 0, access=mmap.ACCESS_READ) as buffer
+    ):
+        if length_bin < 20:
+            return _find_fixed_length(buffer, pw, length_bin)
+        else:
+            return _find_variable_length(buffer, pw)
+
+
+def _find_fixed_length(
+    haystack: mmap.mmap | bytes,
+    needle: bytes,
+    length: int
+) -> bool:
+    # NOTE: our files are sorted, so we perform a binary search
+    #       we can be more efficient for fixed size files, since
+    #       we know where each password is in the file
+    if length >= 20:
+        raise ValueError('Only bins of length below 20 are fixed length')
+
+    stride = length + 1
+    count, remainder = divmod(len(haystack), stride)
+    if remainder:
+        raise ValueError(f'Haystack size is not a multiple of {stride}')
+
+    part = (0, count)
+    prev_part: tuple[int | None, int | None] = (None, None)
+    while part != prev_part:
+        prev_part = part
+        mid = (part[0] + part[1]) // 2
+        start = mid * stride
+        strand = haystack[start:start + length]
+        if needle == strand:
+            return True
+        elif needle < strand:
+            part = (part[0], mid)
+        else:
+            part = (mid + 1, part[1])
+    return False
+
+
+def _find_variable_length(
+    haystack: mmap.mmap | bytes,
+    needle: bytes
+) -> bool:
+    # NOTE: our files are sorted, so we perform a binary search
+    size = len(haystack)
+    part = (0, haystack.rfind(b'\n', 0, size - 1))
+    prev_part: tuple[int | None, int | None] = (None, None)
+    while part != prev_part:
+        prev_part = part
+        mid = (part[0] + part[1]) // 2
+        start = haystack.rfind(b'\n', 0, mid) + 1
+        end = haystack.find(b'\n', mid, size)
+        strand = haystack[start:end]
+        if needle == strand:
+            return True
+        elif needle < strand:
+            part = (part[0], start)
+        else:
+            part = (end + 1, part[1])
+    return False
--- tests/test_bad_passwords.py
+++ tests/test_bad_passwords.py
@@ -1,3 +1,77 @@
 from __future__ import annotations
 
-import bad_passwords
+import pytest
+
+from bad_passwords import _find_fixed_length, is_bad_password
+from secrets import token_urlsafe
+from unittest.mock import patch, MagicMock
+
+
+@pytest.mark.parametrize('password', [
+    '042001ujlf',
+    'vjqgfhjkm1',
+    '03031998abc',
+    'swaggarific',
+    '1029384756jc',
+    'primecarnage',
+    '35050112000EE',
+    'qwertqwert123',
+    '12345234534545',
+    'medizintechnik',
+    '555ooo666ttt111',
+    'zzzzzzzzzzzzzzz',
+    '1l2l3l4l5l6l7l8l',
+    'illneverletyougo',
+    '22222222222222222',
+    'bourbonismyfriend',
+    '5526376269252ltybc',
+    'polymorphomglolwtf',
+    '552708narutouzumaki',
+    'iftdfiftdfiftdf.kbz',
+    '0202J846408E6876906A50441',
+    'fcea920f7412b5da7be0',
+    '08101949Rk1w151008101949rk1',
+    'fedorrambosuperpupersuper',
+    '$HEX[64f66e6572313233]',
+    'MICHAELJITKOmichaeljit!',
+    'BuTTtterflYYyy15FF15',
+    'vjzvfvfcfvfzkexifz19041988',
+    'Basketball.14Basketball.14',
+    'georghoerstrpurtsreohgroeg!',
+])
+def test_bad_passwords(password: str) -> None:
+    assert is_bad_password(password)
+
+
+@pytest.mark.parametrize('length', range(10))
+def test_short_passwords(length: int) -> None:
+    with pytest.warns(UserWarning, match=r'shorter than 10 characters'):
+        assert is_bad_password('1' * length)
+
+
+@patch('bad_passwords.sha1')
+def test_non_existant_bin(sha1: MagicMock) -> None:
+    # we definitely don't have a bin like that
+    sha1.hexdigest.return_value = 'zz'
+    assert not is_bad_password('0123456789')
+
+
+@pytest.mark.parametrize('length', range(10, 30, 2))
+def test_random_passwords(length: int) -> None:
+    # NOTE: While this technically could fail, the chances should be
+    #       astronomically low, if we end up seeing this test flake
+    #       out, we can always deal with it then
+    assert sum(
+        is_bad_password(token_urlsafe(length + it))
+        for it in range(3)
+    ) <= 1
+
+
+def test_find_fixed_length_bad_length() -> None:
+    with pytest.raises(ValueError, match=r'Only bins of length below 20'):
+        _find_fixed_length(b'', b'1' * 20, 20)
+
+
+def test_find_fixed_length_bad_haystack_size() -> None:
+    with pytest.raises(ValueError, match=r'size is not a multiple of 11'):
+        _find_fixed_length(b'1', b'1' * 10, 10)
